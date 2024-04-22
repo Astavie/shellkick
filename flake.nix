1accbd3905d5257c166666fa14be3bc3f24c03fc
Astavie <astavie@pm.me>
2024-04-22 14:35:32 +0200

    update to latest rust

--- flake.nix
+++ flake.nix
@@ -1,27 +1,40 @@
 {
-  inputs.astapkgs.url = "github:Astavie/astapkgs";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.rust-overlay.url = "github:oxalica/rust-overlay";
+  inputs.rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = { self, astapkgs }: astapkgs.lib.package {
-
-    # package = pkgs: with pkgs; ...
-
-    devShell = pkgs: with pkgs; mkShell {
-
-      buildInputs = [
-        dev.rust-nightly
+  outputs = { self, nixpkgs, rust-overlay }: 
+    let
+      overlays = [
+        rust-overlay.overlays.default
+        (final: prev: {
+          rustToolchain = prev.rust-bin.nightly.latest.default.override { extensions = [ "rust-src" ]; };
+        })
       ];
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit overlays system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: with pkgs; {
+        default = pkgs.mkShell {
 
-      LD_LIBRARY_PATH = lib.makeLibraryPath [
-        xorg.libX11
-        xorg.libXcursor
-        xorg.libXrandr
-        xorg.libXi
-        libxkbcommon
-        libGL
-        fontconfig
-      ];
+          buildInputs = [
+            rustToolchain
+          ];
 
+          LD_LIBRARY_PATH = lib.makeLibraryPath [
+            xorg.libX11
+            xorg.libXcursor
+            xorg.libXrandr
+            xorg.libXi
+            libxkbcommon
+            libGL
+            fontconfig
+          ];
+
+        };
+      });
     };
-
-  } [ "x86_64-linux" ];
 }
